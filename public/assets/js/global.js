ba6c594b774ef8c1c4f229b85239290f4702ed4e
unknown <killcoder212@gmail.com>
2017-03-12 12:32:57 +0700

    Fix some layouting

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
+    $('a[href*="#content-"]').click(function(e) {
+        e.preventDefault();
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+                return false;
+            }
+        }
+    });
+
     $('#masuk').click(function(e) {
         e.preventDefault();
         $('#masuk-sistem').modal('show');
@@ -24,14 +38,14 @@ $(document).ready(function() {
     var caption = $('#product-carousel div.item');
     $('.car_left .carousel-title').html(caption.first().find('h1').html());
     $('.car_left .carousel-subtitle').html(caption.first().find('p').html());
-    caption.css('display','none');
+    caption.css('display', 'none');
 
     $('#product-carousel').on('slide.bs.carousel', function(ev) {
-        var caption = $('#product-carousel .item:nth-child(' + ($(ev.relatedTarget).index()+1) + ') .carousel-caption');
+        var caption = $('#product-carousel .item:nth-child(' + ($(ev.relatedTarget).index() + 1) + ') .carousel-caption');
         console.log($(ev.relatedTarget).index());
 
         $('.car_left .carousel-title').html(caption.find('h1').html());
         $('.car_left .carousel-subtitle').html(caption.find('p').html());
-        caption.css('display','none');
+        caption.css('display', 'none');
     });
 });
